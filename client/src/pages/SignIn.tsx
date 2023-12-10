8573e75dec7dbf5ee8c25034da8e5a21abb42465
jerry-hp <jerry17hutariputra>
2023-12-10 16:06:07 +0700

    add sign-in/up validation

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,25 +1,76 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FieldValues, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import api from "../libs/api";
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().required(),
+  })
+  .required();
 
 function SignIn() {
+  const [resMessage, setResMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "all",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleLogin = async (data: FieldValues) => {
+    console.log(data);
+    try {
+      const res = await api.post("/sign-in", data);
+      console.log(res.data);
+      setResMessage(res.data.message);
+      reset();
+    } catch (err: any) {
+      setResMessage(err.response.data.message);
+      reset();
+    }
+  };
+
   return (
     <div>
-      <form action="" className="flex flex-col gap-3 max-w-sm mx-auto mt-20 text-[#EEF4ED] ">
+      <form onSubmit={handleSubmit(handleLogin)} className="flex flex-col gap-3 max-w-sm mx-auto mt-20 text-[#EEF4ED] ">
         <h1 className="text-3xl font-bold text-center text-[#0B2545]">Sign in</h1>
-        <input type="text" placeholder="Email" className="p-2 outline-[#EEF4ED] rounded-lg" />
-        <input type="email" placeholder="Password" className="p-2 outline-[#EEF4ED] rounded-lg" />
-        <button className="bg-[#0B2545] p-2 rounded-lg text-[#EEF4ED] hover:opacity-80">Sign in</button>
+        <input {...register("email")} placeholder="Email" className="p-2 text-[#0B2545] outline-[#EEF4ED] rounded-lg" />
+        {errors.email && <p className="text-red-500">{`${errors.email.message}`}</p>}
+        <input {...register("password")} type="password" placeholder="Password" className="p-2 text-[#0B2545] outline-[#EEF4ED] rounded-lg" />
+        {errors.password && <p className="text-red-500">{`${errors.password.message}`}</p>}
+        <button disabled={isSubmitting} className="bg-[#0B2545] p-2 rounded-lg disabled:opacity-50 text-[#EEF4ED] hover:opacity-80">
+          Sign in
+        </button>
         <div className=" flex gap-2 items-center justify-between before:content-[''] before:block before:h-[1px] before:bg-[#0B2545] before:w-full after:content-[''] after:block after:h-[1px] after:bg-[#0B2545] after:w-full">or</div>
         <button className=" p-2 rounded-lg bg-red-500 hover:opacity-80" type="button">
           Sign in with Google
         </button>
         <p className="text-[#0B2545]">
           Don't have an account?
-          <Link to={"/sign-up"} className="ml-1 text-red-500">
+          <Link to={"/sign-up"} className="ml-1 text-slate-500">
             Sign up
           </Link>
         </p>
       </form>
+      {resMessage && (
+        <div className="absolute p-2 bottom-2 right-[40%] left-[40%] text-center rounded-lg bg-[#0B2545] text-[#EEF4ED]">
+          {resMessage}!
+          <span onClick={() => setResMessage("")} className="cursor-pointer absolute top-[-8px] bg-red-800 w-5 h-5 rounded-full flex justify-center items-center text-[#EEF4ED] right-[-8px] ">
+            x
+          </span>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/pages/SingUp.tsx
+++ client/src/pages/SingUp.tsx
@@ -1,17 +1,70 @@
 import { Link } from "react-router-dom";
-import useSignUp from "../hooks/useSignUp";
+import { FieldValues, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import api from "../libs/api";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const schema = yup
+  .object({
+    username: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().min(6),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password")], "Passwords must match")
+      .required(),
+  })
+  .required();
 
 function SignUp() {
-  const { isError, isLoading, handleChange, handleClick } = useSignUp();
+  const [resMessage, setResMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "all",
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const navigate = useNavigate();
+  const handleSignUp = async (data: FieldValues) => {
+    try {
+      const { confirmPassword, ...dataSignUp } = data;
+      const res = await api.post("/sign-up", dataSignUp);
+      console.log(res);
+      navigate("/sign-in");
+      reset();
+      setResMessage(res.data.message);
+    } catch (err: any) {
+      setResMessage(err.response.data.message);
+      reset();
+    }
+  };
+
   return (
     <div>
-      <form action="" className="flex flex-col gap-3 max-w-sm mx-auto mt-20 text-[-[#0B2545] ">
+      <form onSubmit={handleSubmit(handleSignUp)} className="flex flex-col gap-3 max-w-sm mx-auto mt-20 text-[-[#0B2545] ">
         <h1 className="text-3xl font-bold text-center text-[#0B2545]">Sign up</h1>
-        <input type="text" placeholder="Username" className="p-2 outline-[#EEF4ED] rounded-lg" onChange={handleChange} name="username" />
-        <input type="email" placeholder="Email" className="p-2 outline-[#EEF4ED] rounded-lg" onChange={handleChange} name="email" />
-        <input type="password" placeholder="Password" className="p-2 outline-[#EEF4ED] rounded-lg" onChange={handleChange} name="password" />
-        <button disabled={isLoading} onClick={handleClick} className="bg-[#0B2545] p-2 rounded-lg text-[#EEF4ED] disabled:opacity-25 hover:opacity-80">
-          {isLoading ? "Loading..." : "Sign up"}
+        <input {...register("username")} type="text" placeholder="Username" className="p-2 outline-[#EEF4ED] rounded-lg" />
+        {errors.username && <p className="text-red-500">{`${errors.username.message}`}</p>}
+        <input {...register("email")} type="email" placeholder="Email" className="p-2 outline-[#EEF4ED] rounded-lg" />
+        {errors.email && <p className="text-red-500">{`${errors.email.message}`}</p>}
+        <input {...register("password")} type="password" placeholder="Password" className="p-2 outline-[#EEF4ED] rounded-lg" />
+        {errors.password && <p className="text-red-500">{`${errors.password.message}`}</p>}
+        <input {...register("confirmPassword")} type="password" placeholder="Confirm Password" className="p-2 outline-[#EEF4ED] rounded-lg" />
+        {errors.confirmPassword && <p className="text-red-500">{`${errors.confirmPassword.message}`}</p>}
+        <button disabled={isSubmitting} className="bg-[#0B2545] p-2 rounded-lg text-[#EEF4ED] disabled:opacity-25 hover:opacity-80">
+          {isSubmitting ? "Please wait..." : "Sign up"}
         </button>
         <div className=" flex gap-2 items-center justify-between before:content-[''] before:block before:h-[1px] before:bg-[#0B2545] before:w-full after:content-[''] after:block after:h-[1px] after:bg-[#0B2545] after:w-full">or</div>
         <button className=" p-2 rounded-lg bg-red-500 hover:opacity-80  text-[#EEF4ED]" type="button">
@@ -23,8 +76,15 @@ function SignUp() {
             Sign in
           </Link>
         </p>
-        {isError && <p className="text-red-500">{isError}</p>}
       </form>
+      {resMessage && (
+        <div className="absolute p-2 bottom-2 right-[40%] left-[40%] text-center rounded-lg bg-[#0B2545] text-[#EEF4ED]">
+          {resMessage}!
+          <span onClick={() => setResMessage("")} className="cursor-pointer absolute top-[-8px] bg-red-800 w-5 h-5 rounded-full flex justify-center items-center text-[#EEF4ED] right-[-8px] ">
+            x
+          </span>
+        </div>
+      )}
     </div>
   );
 }
